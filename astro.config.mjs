2945e3a8771a5e52f620a15277cccce9493dba37
Omar Abdulkhalek <abdulkhalekomar.dev@yahoo.com>
2026-03-08 20:10:51 +0100

    site and base is set in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Deine GitHub Pages URL (ohne den Unterordner am Ende)
+  site: 'https://abdulkhalek-omar.github.io',
+  
+  // Der Name deines Repositorys mit Schrägstrichen davor und danach
+  base: '/friseur-salon/',
+});

141c9b79474a3d480d09e3da3bce557a4568c9b3
Omar Abdulkhalek <abdulkhalekomar.dev@yahoo.com>
2026-03-19 11:50:16 +0100

    fix: switch config from GitHub Pages to Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  // Deine GitHub Pages URL (ohne den Unterordner am Ende)
-  site: 'https://abdulkhalek-omar.github.io',
-
-  // Der Name deines Repositorys mit Schrägstrichen davor und danach
-  base: '/friseur-salon/',
-
-  // Verwende den Netlify Adapter für die Bereitstellung auf Netlify
+  site: 'https://friseur-salon-mh.netlify.app',
   output: 'server',
   adapter: netlify()
 });

3cbaee64094bc5082719b7ff4ab4385eeea58bba
Omar Abdulkhalek <abdulkhalekomar.dev@yahoo.com>
2026-03-19 11:35:42 +0100

    chore: update astro to version 6.0.6 and add @astrojs/netlify dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   // Deine GitHub Pages URL (ohne den Unterordner am Ende)
   site: 'https://abdulkhalek-omar.github.io',
-  
+
   // Der Name deines Repositorys mit Schrägstrichen davor und danach
   base: '/friseur-salon/',
+
+  // Verwende den Netlify Adapter für die Bereitstellung auf Netlify
+  output: 'server',
+  adapter: netlify()
 });
